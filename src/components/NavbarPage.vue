3d2f5d150a7a81796fd1a968c5c4573ef1072c7f
Totemy <thetotemofficial@gmail.com>
2024-01-28 20:45:55 +0200

    Adding some page

---
+++ src/components/NavbarPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <button>
+      _hello
+    </button>
+    <button>
+      _about-me
+    </button>
+    <button>
+      _projects
+    </button>
+  </div>
+</template>
+<script>
+export default {}
+</script>
