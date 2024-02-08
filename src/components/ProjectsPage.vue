d217f7493b4bb0e04b975343772a50c659ca663e
Totemy <thetotemofficial@gmail.com>
2024-02-08 17:21:29 +0200

    Added projects page

--- src/components/ProjectsPage.vue
+++ src/components/ProjectsPage.vue
@@ -1,5 +1,43 @@
 <template>
   <div class="projects">
+    <div class="projects__sidebar">
+      <div>
 
+      </div>
+    </div>
+    <div>
+      <div class="projects__items">
+        <div class="projects__item">
+          <div class="projects__header">
+            <h3>E-commerce</h3>
+          </div>
+          <div class="projects__card-body">
+            <img class="projects__item-img" src="/projects/e-commerce.jpg">
+            <div class="projects__item-body">
+              <p>My e-commerce project for sell</p>
+              <button class="btn btn-primary">
+                view-project
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="projects__item">
+          <div class="projects__header">
+            <h3>dashboard</h3>
+          </div>
+          <div class="projects__card-body">
+            <img class="projects__item-img" src="/projects/e-commerce.jpg">
+            <div class="projects__item-body">
+              <p>My e-commerce project for sell</p>
+              <button class="btn btn-primary">
+                view-project
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+<script setup>
+</script>
