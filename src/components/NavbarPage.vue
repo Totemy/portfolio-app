5c84d916cbc3f7454e8fda59435d5852362686b5
Totemy <thetotemofficial@gmail.com>
2024-02-09 22:09:40 +0200

    Edited navbar

--- src/components/NavbarPage.vue
+++ src/components/NavbarPage.vue
@@ -1,22 +1,32 @@
 <template>
   <div class="navbar">
-    <div class="navbar__item">
+    <div class="navbar__item" >
       <router-link to="/"> ihor-stryzhnov</router-link></div>
-    <div  class="navbar__item">
+    <div  class="navbar__item" :class="{ 'active': activeRoute === '/' }">
       <router-link to="/"> _hello</router-link>
     </div>
-    <div class="navbar__item">
+    <div class="navbar__item" :class="{ 'active': activeRoute === '/about' }">
       <router-link to="/about"> _about-me</router-link>
     </div>
-    <div  class="navbar__item">
+    <div  class="navbar__item" :class="{ 'active': activeRoute === '/projects' }">
       <router-link to="/projects"> _projects</router-link>
     </div>
-
-    <div  class="navbar__item">
+    <div  class="navbar__item" :class="{ 'active': activeRoute === '/contact-me' }">
       <router-link to="/contact-me">  _contact-me</router-link>
     </div>
   </div>
 </template>
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      activeRoute: '' // Initialize active route
+    };
+  },
+  watch: {
+    '$route'(to) {
+      this.activeRoute = to.path;
+    }
+  }
+}
 </script>
