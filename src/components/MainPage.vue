f1318840850d7ccb2c50db5ec92a8baf9e4eec3e
Totemy <thetotemofficial@gmail.com>
2024-02-09 22:09:58 +0200

    Edited main page

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -5,13 +5,14 @@
       <h2>Ihor Stryzhnov</h2>
       <h3 style="color:#4D5BCE"> >Front-end developer</h3>
       <div class="main__text">
-        <p>// complete the game to continue</p>
-        <p>// you can also see it on my Github page</p>
+        <p>// find my profile on Github:</p>
         <p> <span style="color: #4D5BCE">const</span>  <span style="color:#43D9AD">githubLink</span>  <span style="color:#FFFFFF">=</span> <a style="color:#E99287" href="https://github.com/Totemy">“https://github.com/Totemy”</a></p>
       </div>
     </div>
     <div class="main__profile">
       <img class="main__img" src="/main/profile.jpg">
+      <div class="main__profile_background">
+      </div>
     </div>
   </div>
 </template>
