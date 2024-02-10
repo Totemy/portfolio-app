a1b8c4416bf0b047f1ce8e72df420d751e5cf319
Totemy <thetotemofficial@gmail.com>
2024-02-10 22:23:58 +0200

    Added mobile footer

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -10,7 +10,10 @@
       <img src="../assets/img/linkedin-fill_white.svg" class="footer__img" alt="logo"/>
     </div>
     <div class="footer__item footer__item-github" >
-      <a href="https://github.com/Totemy">@totemy</a>
+      <a class="footer__item__href_mob" href="https://github.com/Totemy">@totemy</a>
+    </div>
+    <div class="footer__item__mob">
+      <img src="../assets/img/github.svg" class="footer__img" alt="logo"/>
     </div>
   </div>
 </template>
